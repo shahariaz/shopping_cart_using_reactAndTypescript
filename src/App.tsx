793e7e6849486e5533e521a712ea60251cac917c
shahariaz <shahariaz.info@gmail.com>
2024-08-23 19:29:05 +0600

    route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import "./App.css";
-
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Home, Store, About } from "./pages/index";
+import { Navbar } from "./components/index";
 function App() {
   return (
     <>
-      <h1>From App</h1>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap";
+const Navbar = () => {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>Nav</Container>
+    </NavbarBs>
+  );
+};
+
+export default Navbar;
